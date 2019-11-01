f21a779d897431d460b0e4707aa510afaedaae78
moranthomas <thomas@optimizme.com>
2019-11-01 17:40:47 +0000

    hypermedia working

---
+++ demo/src/main/java/com/payroll/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.payroll;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+	
+}
+
---
+++ demo/src/main/java/com/payroll/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,17 @@
+package com.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author thomasmoran
+ *
+ */
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+	}
+}
